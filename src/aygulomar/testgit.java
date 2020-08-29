0d23e538448263f6b3fd48e44e9454952f71bc87
aygul <aygulomarr@gmail.com>
2020-08-28 17:57:26 -0700

    hello

--- src/aygulomar/testgit.java
+++ src/aygulomar/testgit.java
@@ -1,11 +1,12 @@
 package aygulomar;
 
-import org.apache.commons.math3.random.ISAACRandom;
+
 
 public class testgit {
 
 	public static void main(String[] args) {
-		System.out.println("this my first code");
+		String aygulomar;
+		System.out.println("aygulomar");
 
 	}
 
