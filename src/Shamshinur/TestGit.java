0489665b52e521704e88d7b8dd4bb258bce6c06c
shamshinur <idnur4230@gmail.com>
2020-08-30 08:59:35 -0500

    hello, can you look at my code.

--- src/Shamshinur/TestGit.java
+++ src/Shamshinur/TestGit.java
@@ -1,9 +1,12 @@
 package Shamshinur;
 
+import org.apache.commons.math3.random.ISAACRandom;
+
 public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Men Shemshixan");
+		System.out.println("what is going on");
 	}
 
 }
